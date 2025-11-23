f16e8de063b09d7e8276949c3e60beb9c0b1f98a
Your Name <you@example.com>
2025-11-23 23:43:39 +0800

    chain config

--- config/xlayer-mainnet/chain.ts
+++ config/xlayer-mainnet/chain.ts
@@ -3,7 +3,7 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 export const FACTORY_ADDRESS = '0xcce6eb38A4b565300c771f6541451A07a625dfe2'
 
 export const REFERENCE_TOKEN = '0xe538905cf8410324e03a5a23c1c177a474d59b2b'
-export const STABLE_TOKEN_PAIRS = ['0x89608d3dac35d535180f18d2c302a7b4b87f3c75']
+export const STABLE_TOKEN_PAIRS = ['0x89608d3dac35d535180f18d2c302a7b4b87f3c75', '0x3536dea603c3ce2c0256e467906d3f600918237d']
 
 export const TVL_MULTIPLIER_THRESHOLD = '2'
 export const MATURE_MARKET = '1000000'
@@ -21,12 +21,14 @@ export const WHITELIST: string[] = [
   REFERENCE_TOKEN, // WOKB
   '0x74b7f16337b8972027f6196a17a631ac6de26d22', // USDC
   '0x1e4a5963abfd975d8c9021ce480b42188849d41d', // USDT
+  '0x779ded0c9e1022225f8e0630b35a9b54be713736', // USDT0
   '0x5a77f1443d16ee5761d310e38b62f77f726bc71c', // WETH
 ]
 
 export const STABLECOINS: string[] = [
   '0x74b7f16337b8972027f6196a17a631ac6de26d22',
   '0x1e4a5963abfd975d8c9021ce480b42188849d41d',
+  '0x779ded0c9e1022225f8e0630b35a9b54be713736',
 ]
 
 export const SKIP_POOLS: string[] = []
@@ -51,6 +53,12 @@ export const STATIC_TOKEN_DEFINITIONS: TokenDefinition[] = [
     address: Address.fromString('0x1e4a5963abfd975d8c9021ce480b42188849d41d'),
     symbol: 'USDT',
     name: 'Tether USD',
+    decimals: BigInt.fromI32(6),
+  },
+  {
+    address: Address.fromString('0x779ded0c9e1022225f8e0630b35a9b54be713736'),
+    symbol: 'USD₮0',
+    name: 'USD₮0',
     decimals: BigInt.fromI32(6),
   },
 ]
